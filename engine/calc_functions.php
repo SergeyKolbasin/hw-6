cdd29ccf1ccdaad02a298da362dfe4970e695f03
jetsaus <jetsaus@gmail.com>
2020-01-02 01:20:57 +0800

    1.Выполнил 1-е задание

---
+++ engine/calc_functions.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * Функции для работы калькуляторов
+ */
+    require_once '../config/config.php';
+    // Функция сложения
+    function fMathAdd($a = 0, $b = 0) {
+        $result = $a + $b;
+        return $result;
+    }
+    // Функция разности
+    function fMathSub ($a = 0, $b = 0) {
+        $result = $a - $b;
+        return $result;
+    }
+    // Функция умножения
+    function fMathMul ($a = 0, $b = 0) {
+        $result = $a * $b;
+        return $result;
+    }
+    // Функция деления
+    function fMathDiv ($a = 0, $b = 0) {
+        if ($b == 0) {
+            $result = 'на 0 делить нельзя!';
+        } else {
+            $result = $a / $b;
+        }
+        return $result;
+    }
+    // Функция вызова математических операций
+    function fMathOperation(
+        $argA,                  // аргумент A
+        $argB,                  // аргумент B
+        $mathOperation          // математическая операция: "+", "-", "*" или "/"
+    )
+    {
+        switch ($mathOperation) {
+            case '+':                                   // Сложение
+                if (isset($argA) AND isset($argB)) {
+                    return fMathAdd($argA, $argB);
+                }
+                break;
+            case '-':                                   // Разность
+                if (isset($argA) AND isset($argB)) {
+                    return fMathSub($argA, $argB);
+                }
+                break;
+            case '*':                                   // Умножение
+                if (isset($argA) AND isset($argB)) {
+                    return fMathMul($argA, $argB);
+                }
+                break;
+            case '/':                                   // Деление
+                if (isset($argA) AND isset($argB)) {
+                    return fMathDiv($argA, $argB);
+                }
+                break;
+            default:
+                return "Неверно задана операция";
+        }
+    }
+    
---
+++ public/calc_1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Задание №2</title>
+    </head>
+<body>
+<h3>Задача 1:</h3>
+<p>Создать форму-калькулятор c операциями: сложение, вычитание, умножение, деление. Не забыть
+    обработать деление на ноль!:</p>
+<?php
+    require_once '../config/config.php';
+    if (count($_POST)) {
+        // Проверка на существование и инициализация переменных
+        $result = 0;
+        if (isset($_POST['a'])) {
+            $a = $_POST['a'];
+        } else {
+            $a = 0;
+        }
+        if (isset($_POST['b'])) {
+            $b = $_POST['b'];
+        } else {
+            $b = 0;
+        }
+        if (isset($_POST['mathOperation'])) {
+            $mathOperation = MATH_OPERATION[$_POST['mathOperation']];
+        } else {
+            $mathOperation = MATH_OPERATION[0];     // По умолчанию - сложение
+        }
+    }
+    
+    // Вызов функции математических операций
+    if(isset($a) && isset($b)) {
+        $result = fMathOperation($a, $b, $mathOperation);
+    }
+?>
+
+<form method="post">
+    <input type="number" step="any" name="a" value="<?php if (isset($a)) echo $a; ?>">
+    <select name="mathOperation" id="mathOperation">
+        <?php
+            // Отобразим операции калькулятора
+            foreach (MATH_OPERATION as $key => $mathVal) {
+                    // Определим последнюю математическую операцию
+                    if ($mathOperation == $mathVal) {
+                        echo "<option selected value=$key>$mathVal</option>";
+                    } else {
+                        echo "<option value=$key>$mathVal</option>";
+                    }
+                }
+        ?>
+    </select>
+    <input type="number" step="any" name="b" value="<?php if (isset($b)) echo $b; ?>">
+    <input type="submit" value="=">
+    <!--Вывод результата в отдельное поле-->
+    <input type="text" name="result" value="<?php if (isset($result)) echo $result; ?>" readonly>
+</form>
+</body>
+</html>
