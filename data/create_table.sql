94d6fe46f4932756f23753a79b1c936614cd62fa
jetsaus <jetsaus@gmail.com>
2020-01-09 21:38:33 +0800

    3.1.Создана таблица отзывов

--- data/create_table.sql
+++ data/create_table.sql
@@ -1,5 +1,5 @@
-/* Создание таблицы фотографий */
 USE gallery;
+/* Создание таблицы фотографий */
 CREATE TABLE gallery (
 	id smallint unsigned not null auto_increment,		    # id фотографии
 	url varchar(255) not null,								# url
@@ -8,4 +8,12 @@ CREATE TABLE gallery (
 	description varchar(1024),								# описание
 	views smallint,											# количество просмотров
 	constraint pk_id primary key (id)               # первичнй ключ
+);
+/* Создание таблицы отзывов */
+CREATE TABLE review (
+	id smallint unsigned not null,		        # id отзыва, соответствует id животного, если id=0,
+	                                            # то отзыв о самом зоопарке
+	date timestamp default CURRENT_TIMESTAMP,   # дата создания отзыва
+	author varchar(255) not null,		        # автор
+	text text not null      			        # текст отзыва
 );
