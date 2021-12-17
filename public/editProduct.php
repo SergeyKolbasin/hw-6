f9bf915a10f4b3e6b68a00c91541bb23c8db3637
jetsaus <phpsaus@gmail.com>
2021-12-17 22:28:59 +0800

    Создана форма для редактирования товара, кроме фото Файл editProduct.php

--- public/editProduct.php
+++ public/editProduct.php
@@ -10,31 +10,61 @@ if (!$id) {
 }
 $product = getImage($id);
 //die;
+$name = $_POST['name'] ?? $product['name'];
 $description = $_POST['description'] ?? $product['description'];
 $price = $_POST['price'] ?? $product['price'];
-// Проверка, редактировался ли отзыв
-/*
-if ($author !== $review['author'] || $text !== $review['text']) {
-    if ($author && $text) {
-        // Редактируем отзыв
-        if (updateReview($id, $author, $text)) {
-            echo 'Комментарий изменен';
+// Проверка, редактировались ли параметры товара
+if ($name !== $product['name'] || $description !== $product['description'] || $price !== $product['price']) {
+    if ($name && $description && $price) {
+        // Редактируем товар
+        /*
+        if (updateProduct($id, $name, $description, $price)) {
+            echo 'Товар изменен';
         } else {
             echo 'Произошла ошибка';
         }
-    } elseif ($author || $text) {
+        */
+    } elseif ($name || $description || $price) {
         echo 'Форма не заполнена';
     }
 }
 echo '<hr>';
-*/
+var_dump($name);
+var_dump($description);
+var_dump($price);
 ?>
+<!doctype>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <style type="text/css">
+        .img {
+            float: left;
+            margin-right: 1%;
+        }
+    </style>
+    <title><?= $product['name'] ?></title>
+</head>
+<body>
 <h3><?= $product['name'] ?></h3>
+<div class="container">
+    <div class="img">
+        <img src="<?= $product['url'] ?>" alt="<?= $product['name'] ?>">
+    </div>
+</div>
+<br>
+
 <form method="POST">
-    <span>Описание: </span><textarea name="description"><?= $description ?></textarea><br><br>
-    <span>Цена: </span><input type="number" name="author" value="<?= $price ?>"><br><br>
+    <span>Наименование: </span><input type="text" name="name" size="35" value="<?= $name ?>"><br><br>
+    <fielset>
+        <legend>Описание:</legend>
+        <textarea name="description" cols="50" rows="15"><?= $description ?></textarea>
+    </fielset>
+    <br><br>
+    <span>Цена: </span><input type="number" name="price" value="<?= $price ?>" min="0" step="0.01"><br><br>
     <input type="submit" value="Отправить">
 </form>
 <!-- Возврат из формы редактирования -->
-<a href="gallery.php">Обратно в товары</a><br>
+<a href="/gallery.php"><< Назад</a><br>
 <a href="index.php">На главную</a>
+</body>
