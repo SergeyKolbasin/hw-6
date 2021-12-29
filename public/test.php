de8797fddd9ba022fe28d990f6062c09163dc495
jetsaus <phpsaus@gmail.com>
2021-12-30 00:57:15 +0800

    Создана функция получения имени файла в галерее getProductName()

--- public/test.php
+++ public/test.php
@@ -4,7 +4,8 @@
  */
 require_once '../config/config.php';
 
-$arrID = enquireID();
-foreach ($arrID as $key => $val) {
-    echo (int)$val['id'] . ' ';
-}
+// Получение ID для нового товара
+$sql = "SELECT `auto_increment` FROM information_schema.tables WHERE table_schema='" . DB_NAME . "' AND table_name='"
+    . TABLE_PRODUCT . "'";
+$newID = getSingle($sql);
+var_dump((int)$newID['auto_increment']);
